6d19f454924f5f609620ac133beb8e64069fb992
Velpuri Shiva Rama Krishna Chaithanya <2023aa05912@wilp.bits-pilani.ac.in>
2026-06-30 10:22:56 +0530

    third commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     print("Hello from basics-of-git!")
     print("This is a simple Python script to demonstrate basic Git operations.")
-
+    print("You can modify this script and commit your changes to see how Git tracks them.")
+    
 if __name__ == "__main__":
     main()
